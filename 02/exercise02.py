22e208aef2a0e665ab9fa7796d7982cecc317498
Carlos Zanella <cefzanella@gmail.com>
2022-12-03 21:30:00 -0300

    Writing consume_open_tag method (ex 2)

--- 02/exercise02.py
+++ 02/exercise02.py
@@ -1,25 +1,25 @@
 import re
 
+WHITES = set(' \n\r')
+NAME = re.compile(r'^[a-z]\w*$', re.I)
+
 class Node:
-  def __init__(self, tagName, text, children, attributeMap, parent):
+  def __init__(self, tagName = None, text = None, children = None, attributeMap = None, parent = None):
     self.tagName = tagName
     self.text = text
     self.children = children
     self.attributeMap = attributeMap
     self.parent = parent
 
-whites = set(' \n\r')
-numbers = set('0123456789.')
-attribute_name = re.compile(r'^[a-z]\w*$', re.I)
-
 class ParsingException(Exception):
-  def __init__(self, message, cursor):
+  def __init__(self, message, parser):
     super()
     self.message = message
-    self.cursor = cursor
+    self.parser = parser
 
   def __str__(self):
-    return '{} on position {}'.format(self.message, self.cursor)
+    return '{} on position {}'.format(self.message, self.parser.cursor)
+
 
 class HTMLParser:
   def __init__(self, doc):
@@ -33,11 +33,11 @@ class HTMLParser:
     return self.doc[self.cursor]
 
   def skip_whites(self):
-    while not self.ended() and self.char() in whites:
+    while not self.ended() and self.char() in WHITES:
       self.cursor += 1
 
   def skip_non_whites(self):
-    while not self.ended() and self.char() not in whites:
+    while not self.ended() and self.char() not in WHITES:
       self.cursor += 1
 
   def consume_word(self):
@@ -69,33 +69,24 @@ class HTMLParser:
     end = self.cursor
 
     if not self.consume_token(opener):
-      raise ParsingException('Expected closing {}'.format(opener), self.cursor)
+      raise ParsingException('Expected closing {}'.format(opener), self)
 
     return self.doc[start:end]
 
   def consume_number(self):
-    self.skip_whites()
     start = self.cursor
-    self.consume_token('-')
-    dot_found = False
-    while not self.ended() and self.char() in numbers:
-      if self.char() == '.':
-        if dot_found:
-          raise ParsingException('Unexpected .', self.cursor)
-        dot_found = True
-      self.cursor += 1
-
-    if start == self.cursor or self.doc[self.cursor - 1] == '-':
+    try:
+      value = float(self.consume_word())
+    except:
       self.cursor = start
       return None
 
-    end = self.cursor
-    return float(self.doc[start:end])
+    return value
 
   def consume_attribute(self):
     start = self.cursor
     name = self.consume_word()
-    if not attribute_name.match(name):
+    if not NAME.match(name):
       self.cursor = start
       return None
 
@@ -104,11 +95,23 @@ class HTMLParser:
     else:
       value = self.consume_string() or self.consume_number()
       if not value:
-        raise ParsingException('Expected string or number', self.cursor)
+        raise ParsingException('Expected string or number', self)
 
     return (name, value)
 
-parser = HTMLParser(' foo = 23 bar >')
+  def consume_open_tag(self):
+    if not self.consume_token('<'):
+      return None
+
+    self.skip_whites()
+    start = self.cursor
+    name = self.consume_word()
+    if not NAME.match(name):
+      self.cursor = start
+      raise ParsingException('Invalid tag name', self)
+
+    
+parser = HTMLParser(' foo = +-.23 bar >')
 print(parser.consume_attribute())
 print(parser.consume_attribute())
 print(parser.consume_token('>'))
